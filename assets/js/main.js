435b8a6d0411359ecc2f3cb2df6f6d78a5453e9f
orianig <oriana.ig016@gmail.com>
2023-09-01 00:15:05 +0200

    feat: add card functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@ const getInfluencerList = async () => {
         influencersList = [
             data, data, data, data, data, data,
         ];
+        // influencer = data
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -26,8 +27,26 @@ const generateInfluencerListHTML = () => {
     influencersList.map(influencer => {
         let cardHTML = document.createElement('article');
         cardHTML.classList.add('custom-card', 'd-flex', 'flex-row', 'position-relative');
-        cardHTML.innerHTML = 'kaka'
+        // * bars button
+        let barsButtonHTML = document.createElement('div');
+        barsButtonHTML.classList.add('position-absolute', 'top-0', 'end-0', 'm-2');
+        barsButtonHTML.innerHTML='<i class="fa-solid fa-bars"></i>';
+        cardHTML.appendChild(barsButtonHTML);
+
+        // * card left side
+        let cardLeftSideHTML = document.createElement('div');
+        cardLeftSideHTML.innerHTML = `<span class='cosita linda'>${influencer.influcard
+            .username}</span><p>${influencer.influcard.username}</p>`;
+        cardHTML.appendChild(cardLeftSideHTML);
+
+        // * card right side
+        let cardRightSideHTML = document.createElement('div');
+        cardRightSideHTML.innerHTML = '<span>este es el lado der</span>';
+        cardHTML.appendChild(cardRightSideHTML);
+
         cardsContainerHTML.appendChild(cardHTML);
+
+
     });
 
 }
